5c448ac5254b4dc748803408946d9c4b022ec1ea
alexander <drommefangerr@gmail.com>
2024-10-13 20:36:08 +0300

    Initial commit

---
+++ FPone/src/FileManager.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class FileManager {
+
+  private String pathToFile;
+
+  public FileManager (String pathToFile){
+      this.pathToFile = pathToFile;
+  }
+
+
+
+
+}
---
+++ FPone/src/MainApp.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MainApp {
+
+
+    public static void main(String[] args) {
+
+
+        FileManager inputFile = new FileManager("E:\\Learning\\learn_java\\~JR final project\\FP1\\FPone\\src\\testInputFile.txt");
+
+
+
+    }
+}
